f070ab37dfbe4930b205f35215568531c60d51d4
Klecia Vladmaker <klecia.vladmaker@hotmail.com>
2021-01-29 16:06:18 -0300

    Entity User e REST funcionando

--- src/main/java/com/devsuperior/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/devsuperior/workshopmongo/WorkshopmongoApplication.java
@@ -1,4 +1,4 @@
-package com.example.devsuperior.workshopmongo;
+package com.devsuperior.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
